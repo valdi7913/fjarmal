87393d9b5327feeeafa88e553507a9bb3f353d40
valdi791 <valdi7913@gmail.com>
2024-12-28 12:32:22 +0000

    fix: add savings graph

--- src/js/mortgage.mjs
+++ src/js/mortgage.mjs
@@ -1,185 +1,176 @@
 export default class Mortgage {
-	/**
-	 * @param {float} principal
-	 * @param {float} interest_rate expressed as a yearly percentage X%
-	 * @param {integer} length_in_years expressed as a whole number of years
-	 * @param {boolean} [insured = false] boolean flag dictates whether loan is CPI backed
-	 * @param {boolean} [equal_payments = false] equal_payments
-	 * @param {float} [inflation = 0] expressed as a yearly percentage X%
-	 */
-	constructor(
-		principal,
-		interest_rate,
-		length_in_years,
-		insured = false,
-		equal_payments = false,
-		inflation = 0
-	) {
-		this.principal = parseInt(principal);
-		this.interest_rate = parseFloat(interest_rate) / 1200; //converted to monthly interest as a decimal
-		this.length = parseFloat(length_in_years) * 12; //converted to number of payments
-		this.is_insured = insured;
-		this.is_equal_payments = equal_payments;
-		this.inflation = parseFloat(inflation) / 1200; //converted to monthy inflation as a decimal
+  /**
+   * @param {float} principal
+   * @param {float} interest_rate expressed as a yearly percentage X%
+   * @param {integer} length_in_years expressed as a whole number of years
+   * @param {boolean} [insured = false] boolean flag dictates whether loan is CPI backed
+   * @param {boolean} [equal_payments = false] equal_payments
+   * @param {float} [inflation = 0] expressed as a yearly percentage X%
+   */
+  constructor(
+    principal,
+    interest_rate,
+    length_in_years,
+    insured = false,
+    equal_payments = false,
+    inflation = 0,
+  ) {
+    this.principal = parseInt(principal);
+    this.interest_rate = parseFloat(interest_rate) / 1200; //converted to monthly interest as a decimal
+    this.length = parseFloat(length_in_years) * 12; //converted to number of payments
+    this.is_insured = insured;
+    this.is_equal_payments = equal_payments;
+    this.inflation = parseFloat(inflation) / 1200; //converted to monthy inflation as a decimal
 
-		this.calculateLoan();
-	}
+    this.calculateLoan();
+  }
 
-	calculateLoan() {
-		// Initialize loan variables
-		this.months = [];
-		this.interest_history = [0];
-		this.installment_history = [0];
-		this.principal_history = [this.principal];
+  calculateLoan() {
+    // Initialize loan variables
+    this.months = [];
+    this.interest_history = [0];
+    this.installment_history = [0];
+    this.principal_history = [this.principal];
 
-		// Calculate the corresponding loan type
-		if (this.is_equal_payments && this.is_insured) this.equalPaymentCpi();
-		else if (this.is_equal_payments && !this.is_insured)
-			this.equalPayments();
-		else if (!this.is_equal_payments && this.is_insured)
-			this.equalInstallmentsCpi();
-		else if (!this.is_equal_payments && !this.is_insured)
-			this.equalInstallments();
-	}
+    // Calculate the corresponding loan type
+    if (this.is_equal_payments && this.is_insured) this.equalPaymentCpi();
+    else if (this.is_equal_payments && !this.is_insured) this.equalPayments();
+    else if (!this.is_equal_payments && this.is_insured)
+      this.equalInstallmentsCpi();
+    else if (!this.is_equal_payments && !this.is_insured)
+      this.equalInstallments();
+  }
 
-	equalPayments() {
-		let m =
-			(this.principal *
-				this.interest_rate *
-				Math.pow(1 + this.interest_rate, this.length)) /
-			(Math.pow(1 + this.interest_rate, this.length) - 1);
+  equalPayments() {
+    let m =
+      (this.principal *
+        this.interest_rate *
+        Math.pow(1 + this.interest_rate, this.length)) /
+      (Math.pow(1 + this.interest_rate, this.length) - 1);
 
-		for (let month = 1; month <= this.length; month++) {
-			this.months[month] = month;
-			this.principal_history[month] =
-				this.principal_history[month - 1] * (1 + this.interest_rate) -
-				m;
-			this.interest_history[month] =
-				this.interest_rate * this.principal_history[month - 1];
-			this.installment_history[month] = m - this.interest_history[month];
-		}
+    for (let month = 1; month <= this.length; month++) {
+      this.months[month] = month;
+      this.principal_history[month] =
+        this.principal_history[month - 1] * (1 + this.interest_rate) - m;
+      this.interest_history[month] =
+        this.interest_rate * this.principal_history[month - 1];
+      this.installment_history[month] = m - this.interest_history[month];
+    }
+  }
 
-		console.log(m);
-		console.log(this.installment_history);
-		console.log(this.interest_history);
-	}
+  equalInstallments() {
+    for (let month = 1; month <= this.length; month++) {
+      this.months[month] = month;
+      this.principal_history[month] =
+        this.principal * (1 - month / this.length);
+      this.installment_history[month] = this.principal / this.length;
+      this.interest_history[month] =
+        this.principal_history[month - 1] * this.interest_rate;
+    }
+  }
 
-	equalInstallments() {
-		for (let month = 1; month <= this.length; month++) {
-			this.months[month] = month;
-			this.principal_history[month] =
-				this.principal * (1 - month / this.length);
-			this.installment_history[month] = this.principal / this.length;
-			this.interest_history[month] =
-				this.principal_history[month - 1] * this.interest_rate;
-		}
-	}
+  equalPaymentCpi() {
+    let m =
+      (this.principal *
+        this.interest_rate *
+        Math.pow(1 + this.interest_rate, this.length)) /
+      (Math.pow(1 + this.interest_rate, this.length) - 1);
+    for (let month = 1; month <= this.length; month++) {
+      this.months[month] = month;
+      this.principal_history[month] =
+        this.principal_history[month - 1] *
+          (1 + this.interest_rate) *
+          (1 + this.inflation) -
+        m * Math.pow(1 + this.inflation, month);
+      this.interest_history[month] =
+        this.interest_rate * this.principal_history[month - 1];
+      this.installment_history[month] =
+        Math.pow(1 + this.inflation, month) * m - this.interest_history[month];
+    }
+  }
 
-	equalPaymentCpi() {
-		let m =
-			(this.principal *
-				this.interest_rate *
-				Math.pow(1 + this.interest_rate, this.length)) /
-			(Math.pow(1 + this.interest_rate, this.length) - 1);
-		for (let month = 1; month <= this.length; month++) {
-			this.months[month] = month;
-			this.principal_history[month] =
-				this.principal_history[month - 1] *
-					(1 + this.interest_rate) *
-					(1 + this.inflation) -
-				m * Math.pow(1 + this.inflation, month);
-			this.interest_history[month] =
-				this.interest_rate * this.principal_history[month - 1];
-			this.installment_history[month] =
-				Math.pow(1 + this.inflation, month) * m -
-				this.interest_history[month];
-		}
-	}
+  equalInstallmentsCpi() {
+    for (let month = 1; month <= this.length; month++) {
+      this.months[month] = month;
+      this.principal_history[month] =
+        this.principal *
+        (1 + this.inflation) ** month *
+        (1 - month / this.length);
+      this.installment_history[month] =
+        (this.principal * (1 + this.inflation) ** month) / this.length;
+      this.interest_history[month] =
+        this.principal_history[month - 1] * this.interest_rate;
+    }
+  }
 
-	equalInstallmentsCpi() {
-		for (let month = 1; month <= this.length; month++) {
-			this.months[month] = month;
-			this.principal_history[month] =
-				this.principal *
-				(1 + this.inflation) ** month *
-				(1 - month / this.length);
-			this.installment_history[month] =
-				(this.principal * (1 + this.inflation) ** month) / this.length;
-			this.interest_history[month] =
-				this.principal_history[month - 1] * this.interest_rate;
-		}
-	}
+  // Getters
+  getFirstPayment() {
+    return this.installment_history[0] + this.interest_history[0];
+  }
 
-	// Getters
-	getFirstPayment() {
-		return this.installment_history[0] + this.interest_history[0];
-	}
+  getTotalInterestPaid() {
+    const sum = this.interest_history.reduce(
+      (sum, interest) => sum + interest,
+      0,
+    );
+    return Math.round(sum);
+  }
 
-	getTotalInterestPaid() {
-		const sum = this.interest_history.reduce(
-			(sum, interest) => sum + interest,
-			0
-		);
-		return Math.round(sum);
-	}
+  getTotalPrincipalPayments() {
+    const sum = this.installment_history.reduce(
+      (sum, interest) => sum + interest,
+      0,
+    );
 
-	getTotalPrincipalPayments() {
-		const sum = this.installment_history.reduce(
-			(sum, interest) => sum + interest,
-			0
-		);
+    return Math.round(sum);
+  }
 
-		return Math.round(sum);
-	}
+  getTotalPaid() {
+    return this.getTotalInterestPaid() + this.getTotalPrincipalPayments();
+  }
 
-	getTotalPaid() {
-		return this.getTotalInterestPaid() + this.getTotalPrincipalPayments();
-	}
+  getPlotablePrincipal() {
+    return this.principal_history;
+  }
 
-	getPlotablePrincipal() {
-		return this.principal_history;
-	}
+  getPlotableInstallments() {
+    return this.installment_history;
+  }
 
-	getPlotableInstallments() {
-		return this.installment_history;
-	}
+  getPlotableInterest() {
+    return this.interest_history;
+  }
 
-	getPlotableInterest() {
-		return this.interest_history;
-	}
+  getPlotableRollingInstallments() {
+    let data = [this.installment_history[0]];
+    for (let i = 1; i < this.installment_history.length; i++) {
+      data[i] = this.installment_history[i] + data[i - 1];
+    }
+    return data;
+  }
 
-	getPlotableRollingInstallments() {
-		let data = [this.installment_history[0]];
-		for (let i = 1; i < this.installment_history.length; i++) {
-			data[i] = this.installment_history[i] + data[i - 1];
-		}
-		return data;
-	}
+  getPlotableRollingInterest() {
+    let data = [this.interest_history[0]];
+    for (let i = 1; i < this.interest_history.length; i++) {
+      data[i] = this.interest_history[i] + data[i - 1];
+    }
+    return data;
+  }
 
-	getPlotableRollingInterest() {
-		let data = [this.interest_history[0]];
-		for (let i = 1; i < this.interest_history.length; i++) {
-			data[i] = this.interest_history[i] + data[i - 1];
-		}
-		return data;
-	}
+  getPlotablePayments() {
+    let data = [];
+    for (let i = 0; i < this.interest_history.length; i++) {
+      data[i] = this.interest_history[i] + this.installment_history[i];
+    }
+    return data;
+  }
 
-	getPlotablePayments() {
-		let data = [];
-		for (let i = 0; i < this.interest_history.length; i++) {
-			data[i] = this.interest_history[i] + this.installment_history[i];
-		}
-		return data;
-	}
-
-	getPlotableRollingPayments() {
-		let data = [this.interest_history[0] + this.installment_history[0]];
-		for (let i = 1; i < this.interest_history.length; i++) {
-			data[i] =
-				this.interest_history[i] +
-				this.installment_history[i] +
-				data[i - 1];
-		}
-		return data;
-	}
+  getPlotableRollingPayments() {
+    let data = [this.interest_history[0] + this.installment_history[0]];
+    for (let i = 1; i < this.interest_history.length; i++) {
+      data[i] =
+        this.interest_history[i] + this.installment_history[i] + data[i - 1];
+    }
+    return data;
+  }
 }
--- src/js/situation.js
+++ src/js/situation.js
@@ -1,234 +1,199 @@
 class Situation {
-	constructor(
-		currentSavings,
-		currentIncome,
-		currentCosts,
-		yearlyROI,
-		yearlyPromotion,
-		loanRatio
-	) {
-		this.currentSavings = currentSavings;
-		this.currentIncome = currentIncome;
-		this.currentCosts = currentCosts;
-		this.yearlyROI = yearlyROI;
-		this.yearlyPromotion = yearlyPromotion;
+  constructor(
+    currentSavings,
+    currentIncome,
+    currentCosts,
+    yearlyROI,
+    yearlyPromotion,
+    loanRatio,
+  ) {
+    this.currentSavings = currentSavings;
+    this.currentIncome = currentIncome;
+    this.currentCosts = currentCosts;
+    this.yearlyROI = yearlyROI;
+    this.yearlyPromotion = yearlyPromotion;
 
-		this.loanRatio = loanRatio;
+    this.loanRatio = loanRatio;
 
-		this.savingsHistory = [];
-		this.incomeHistory = [];
-		this.maxLoan = [];
-		this.years = [];
-	}
+    this.savingsHistory = [];
+    this.incomeHistory = [];
+    this.maxLoan = [];
+    this.years = [];
+  }
 
-	calculate(years) {
-		this.savingsHistory = [currentSavings];
-		this.incomeHistory = [this.income];
-		this.maxLoan = [];
+  calculate(years) {
+    this.savingsHistory = [currentSavings];
+    this.incomeHistory = [this.income];
+    this.maxLoan = [];
 
-		for (let i = 0; i < years * 12; i++) {
-			let newSavingsBalance = this.savingsHistory[i];
-			let interestIncome = (this.savingsHistory[i] * this.yearlyROI) / 12;
-			newSavingsBalance +=
-				this.currentIncome + interestIncome - this.currentCosts;
+    for (let i = 0; i < years * 12; i++) {
+      let newSavingsBalance = this.savingsHistory[i];
+      let interestIncome = (this.savingsHistory[i] * this.yearlyROI) / 12;
+      newSavingsBalance +=
+        this.currentIncome + interestIncome - this.currentCosts;
 
-			this.years.push(i / 12);
-			this.incomeHistory.push(interestIncome + this.currentIncome);
-			this.savingsHistory.push(newSavingsBalance);
-			this.maxLoan.push(
-				newSavingsBalance +
-					maxPrincipalLoan(
-						this.loanRatio,
-						15,
-						0.1075,
-						this.currentIncome
-					)
-			);
-		}
-	}
+      this.years.push(i / 12);
+      this.incomeHistory.push(interestIncome + this.currentIncome);
+      this.savingsHistory.push(newSavingsBalance);
+      this.maxLoan.push(
+        newSavingsBalance +
+          maxPrincipalLoan(this.loanRatio, 15, 0.1075, this.currentIncome),
+      );
+    }
+  }
 
-	timeToTarget(target) {
-		for (let index = 0; index < this.savingsHistory.length; index++) {
-			if (this.savingsHistory[index] >= target) {
-				return index;
-			}
-		}
-	}
+  timeToTarget(target) {
+    for (let index = 0; index < this.savingsHistory.length; index++) {
+      if (this.savingsHistory[index] >= target) {
+        return index;
+      }
+    }
+  }
 
-	timeToTargetWithLoan(target) {
-		for (let index = 0; index < this.savingsHistory.length; index++) {
-			if (this.maxLoan[index] >= target) {
-				return index;
-			}
-		}
-	}
+  timeToTargetWithLoan(target) {
+    for (let index = 0; index < this.savingsHistory.length; index++) {
+      if (this.maxLoan[index] >= target) {
+        return index;
+      }
+    }
+  }
 
-	plotSavings() {
-		let data = [];
-		for (let i = 0; i < this.savingsHistory.length * 12; i++) {
-			data.push({ x: i / 12, y: this.savingsHistory[i] });
-		}
-		return data;
-	}
+  plotSavings() {
+    let data = [];
+    for (let i = 0; i < this.savingsHistory.length * 12; i++) {
+      data.push({ x: i / 12, y: this.savingsHistory[i] });
+    }
+    return data;
+  }
+
+  plotMaxmimumLoan() {
+    let data = [];
+    for (let i = 0; i < this.maxLoan.length * 12; i++) {
+      data.push({ x: i / 12, y: this.maxLoan[i] });
+    }
+    return data;
+  }
 }
 
 const ctx = document.getElementById("my-chart");
-const ratioLabel = document.getElementById("ratioLabel");
+
 const ratioInput = document.getElementById("ratioInput");
-ratioLabel.innerText = ratioInput.value + " %";
+ratioInput.min = 0;
+ratioInput.value = 0.5;
+ratioInput.step = 0.01;
+ratioInput.oninput = replot;
 
-const currentSavingsLabel = document.getElementById("savingsLabel");
 const currentSavingsInput = document.getElementById("savingsInput");
-currentSavingsLabel.innerText = formatNumberWithSeparators(
-	currentSavingsInput.value
-);
+currentSavingsInput.min = 0;
+currentSavingsInput.value = 1_000_000;
+currentSavingsInput.step = 100_000;
+currentSavingsInput.oninput = replot;
 
-const currentIncomeLabel = document.getElementById("incomeLabel");
 const currentIncomeInput = document.getElementById("incomeInput");
-currentIncomeLabel.innerText = formatNumberWithSeparators(
-	currentIncomeInput.value
-);
+currentIncomeInput.min = 0;
+currentIncomeInput.value = 500_000;
+currentIncomeInput.step = 10_000;
+currentIncomeInput.oninput = replot;
 
-const yearLabel = document.getElementById("yearLabel");
 const yearInput = document.getElementById("yearInput");
-yearLabel.innerText = yearInput.value;
+yearInput.min = 1;
+yearInput.max = 40;
+yearInput.value = 25;
+yearInput.step = 1;
+yearInput.oninput = replot;
 
-const targetLabel = document.getElementById("targetLabel");
 const targetInput = document.getElementById("targetInput");
-targetLabel.innerText = targetInput.value;
+targetInput.min = 0;
+targetInput.value = 50_000_000;
+targetInput.step = 100_000;
+targetInput.oninput = replot;
 
 let chart = plot();
 
-function currentSavingsChanged() {
-	currentSavingsLabel.innerHTML = formatNumberWithSeparators(
-		currentSavingsInput.value
-	);
-	chart.destroy();
-	chart = plot();
-}
-
-function loanRatioChanged() {
-	ratioLabel.innerHTML = ratioInput.value + " %";
-	chart.destroy();
-	chart = plot();
-}
-
-function currentIncomeChanged() {
-	currentIncomeLabel.innerHTML = formatNumberWithSeparators(
-		currentIncomeInput.value
-	);
-	chart.destroy();
-	chart = plot();
-}
-
-function yearChanged() {
-	yearLabel.innerHTML = yearInput.value;
-	chart.destroy();
-	chart = plot();
-}
-
-function targetChanged() {
-	targetLabel.innerHTML = formatNumberWithSeparators(targetInput.value);
-	chart.destroy();
-	chart = plot();
+function replot() {
+  chart.destroy();
+  chart = plot();
 }
 
 function maxPrincipalLoan(ratio, years, interestRate, income) {
-	let oneOverNumberOfMonths = 1 / (years * 12);
-	let monthlyInterestRate = interestRate / 12;
-	return (ratio * income) / (oneOverNumberOfMonths + monthlyInterestRate);
-}
-
-function zip(x, y) {
-	let data = [];
-
-	if (x.length !== y.length) {
-		throw new Error(
-			"Lists must be equal length but are of length x:" +
-				x.length +
-				" y:" +
-				y.length
-		);
-	}
-
-	for (let i = 0; i < x.length; i++) {
-		data.push({ x: x[i], y: y[i] });
-	}
-	return data;
+  let oneOverNumberOfMonths = 1 / (years * 12);
+  let monthlyInterestRate = interestRate / 12;
+  return (ratio * income) / (oneOverNumberOfMonths + monthlyInterestRate);
 }
 
 function plot() {
-	let me = new Situation(
-		(currentSavings = parseInt(currentSavingsInput.value)),
-		(currentIncome = parseInt(currentIncomeInput.value)),
-		(currentCosts = 120000),
-		(yearlyROI = 0.0865),
-		(yearlyPromotion = 0.05),
-		(loanRatio = parseFloat(ratioInput.value))
-	);
+  let me = new Situation(
+    (currentSavings = parseInt(currentSavingsInput.value)),
+    (currentIncome = parseInt(currentIncomeInput.value)),
+    (currentCosts = 120000),
+    (yearlyROI = 0.0865),
+    (yearlyPromotion = 0.05),
+    (loanRatio = parseFloat(ratioInput.value)),
+  );
 
-	let years = parseInt(yearInput.value);
-	me.calculate(years);
-	let savings = me.plotSavings();
+  const years = parseInt(yearInput.value);
+  me.calculate(years);
+  const savings = me.plotSavings();
+  const maxLoans = me.plotSavings();
 
-	let target = parseInt(targetInput.value);
+  let target = parseInt(targetInput.value);
+  console.log("Savings ", savings);
+  console.log("MaxLoans ", maxLoans);
 
-	return new Chart(ctx, {
-		type: "line",
-		data: {
-			datasets: [
-				{
-					label: "Target",
-					data: [{ x: me.timeToTarget(target) / 12, y: target }],
-					backgroundColor: "#002F6C",
-				},
-				{
-					label: "Target með láni",
-					data: [
-						{ x: me.timeToTargetWithLoan(target) / 12, y: target },
-					],
-					backgroundColor: "#007B82",
-				},
-				{
-					label: "Sparnaður",
-					data: savings,
-					pointRadius: 1,
-					fill: {
-						target: "origin",
-					},
-					backgroundColor: "#4CAF50",
-				},
-				{
-					label: "Sparnaður + lán",
-					data: zip(me.years, me.maxLoan),
-					pointRadius: 1,
-					fill: {
-						target: "origin",
-					},
-					backgroundColor: "#FFA500",
-				},
-			],
-		},
-		options: {
-			animation: false,
-			scales: {
-				x: {
-					type: "linear",
-					position: "bottom",
-					title: {
-						display: true,
-						text: "Ár",
-					},
-					max: years,
-				},
-				y: {
-					title: {
-						display: true,
-						text: "Upphæð í kr.",
-					},
-					min: 0,
-				},
-			},
-		},
-	});
+  return new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Target",
+          data: [{ x: me.timeToTarget(target) / 12, y: target }],
+          backgroundColor: "#002F6C",
+        },
+        {
+          label: "Target með láni",
+          data: [{ x: me.timeToTargetWithLoan(target) / 12, y: target }],
+          backgroundColor: "#007B82",
+        },
+        {
+          label: "Sparnaður",
+          data: savings,
+          pointRadius: 1,
+          fill: {
+            target: "origin",
+          },
+          backgroundColor: "#4CAF50",
+        },
+        {
+          label: "Sparnaður + lán",
+          data: maxLoans,
+          pointRadius: 1,
+          fill: {
+            target: "origin",
+          },
+          backgroundColor: "#FFA500",
+        },
+      ],
+    },
+    options: {
+      animation: false,
+      scales: {
+        x: {
+          type: "linear",
+          position: "bottom",
+          title: {
+            display: true,
+            text: "Ár",
+          },
+          max: years,
+        },
+        y: {
+          title: {
+            display: true,
+            text: "Upphæð í kr.",
+          },
+          min: 0,
+        },
+      },
+    },
+  });
 }
